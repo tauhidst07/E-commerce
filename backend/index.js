8541b483125a4b23e45eb6386116c655afd2c783
tauhidst07 <atauhid07@gmail.com>
2025-10-04 12:25:18 +0530

    fix app start

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,50 @@
-const express = require("express");   
-const mongoose = require("mongoose"); 
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const app = express(); 
-const PORT = process.env.PORT || 3000;    
+const app = express();
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
-const authRoute = require("./routes/User"); 
-const productRoute = require("./routes/Product"); 
+const authRoute = require("./routes/User");
+const productRoute = require("./routes/Product");
 const orderRoute = require("./routes/Order");
 app.use(cors());
-app.use(express.json()); 
-app.use("/api/auth",authRoute); 
-app.use("/api/products",productRoute);  
-app.use("/api/order",orderRoute);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
+app.use("/api/order", orderRoute);
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("server in running");
-}) 
+})
 
- 
+
 //global error handler
-app.use((err,req,res,next)=>{
-    if(err.code === 'LIMIT_FILE_SIZE'){
-       return res.status(413).json({
-            message:"file too large ,max-size is 5 mb"
+app.use((err, req, res, next) => {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+        return res.status(413).json({
+            message: "file too large ,max-size is 5 mb"
         })
-    } 
-    res.status(500).json({ 
-        message:err.message, 
+    }
+    res.status(500).json({
+        message: err.message,
         err
-    }) 
-    console.log("err: ",err);
-})
-
-
-app.listen(PORT,()=>{
-    mongoose.connect(process.env.DB_URL).then(()=>{
-        console.log("db connected")
     })
-    .catch((err)=>{
-        console.log("db error ",err);
-    });
+    console.log("err: ", err);
 })
+
+
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log("DB connected");
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("DB connection error: ", err);
+    });
